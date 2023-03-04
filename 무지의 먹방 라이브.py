e598e4ba19786079bbf23334119f0da982fdd8e8
skqls <kitchin105@gmail.com>
2023-03-04 16:11:47 +0900

    20230304

---
+++ 무지의 먹방 라이브.py
@@ -0,0 +1,35 @@
+# 2019 KAKAO BLIND RECRUITMENT
+# 무지의 먹방 라이브
+
+
+import heapq
+
+def solution(food_times, k):
+    
+    
+    if sum(food_times) <= k :
+        return -1
+    else :
+        
+        q = []
+        for i in range(len(food_times)):
+            heapq.heappush(q,(food_times[i],i+1))
+        
+        length = len(food_times)
+        prev = 0 
+        sum_num = 0
+        
+        while ((q[0][0]-prev)*length + sum_num) <= k :
+            now = heapq.heappop(q)[0]
+            sum_num += (now-prev)*length
+            length -= 1
+            prev = now
+            
+        q.sort(key = lambda x : x[1])
+        k -= sum_num
+        return q[(k%length)][1]
+        
+        
+# food_times = [3, 1, 2]
+# k = 5
+# solution(food_times, k)
